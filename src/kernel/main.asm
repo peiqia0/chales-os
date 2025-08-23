9758e71a003b79729981207171f844d45d5f4171
chales <76992137+peiqia0@users.noreply.github.com>
2025-08-23 21:20:52 +1000

    initial commit added everything
    added everything

---
+++ src/kernel/main.asm
@@ -0,0 +1,38 @@
+org 0x0
+bits 16
+
+
+%define ENDL 0x0D, 0x0A
+
+
+start:
+    mov si, msg_hello
+    call puts
+
+.halt:
+    cli
+    hlt
+
+puts:
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb
+    and al, al
+    jz .done
+
+    mov ah, 0x0E
+    mov bh, 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
+
+msg_hello: db 'Hello world!', ENDL, 0
